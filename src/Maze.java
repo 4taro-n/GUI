d04da154c604dee95bfcd13d98376282caa9fdf5
Haotian Ouyang <1319310146@qq.com>
2022-06-06 14:20:31 +1000

    Added Maze Class

---
+++ src/Maze.java
@@ -0,0 +1,72 @@
+import java.io.Serializable;
+
+public class Maze implements Comparable<Maze>, Serializable {
+
+    private String mazeName;
+
+    private String author;
+
+    private String dateCreated;
+
+    private String dateEdited;
+
+    private int mazeID;
+
+    public Maze(){}
+
+    public Maze(String mazeName, String author, int mazeID, String dateCreated, String dateEdited){
+        this.mazeName = mazeName;
+        this.author = author;
+        this.mazeID = mazeID;
+        this.dateCreated = dateCreated;
+        this.dateEdited = dateEdited;
+    }
+
+    public String getMazeName(){
+        return mazeName;
+    }
+
+    public void setMazeName(String mazeName){
+        this.mazeName = mazeName;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getDateCreated(){
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated){
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateEdited(){
+        return dateEdited;
+    }
+
+    public void setDateEdited(String dateEdited){
+        this.dateEdited = dateEdited;
+    }
+
+    public int getMazeID(){
+        return mazeID;
+    }
+
+    public void setMazeID(int mazeID){
+        this.mazeID = mazeID;
+    }
+
+    @Override
+    public int compareTo(Maze o) {
+        return this.mazeName.compareTo(o.mazeName);
+    }
+    public String toString(){
+        return mazeName + " " + author + " " + mazeID + " " + dateCreated + " "+ dateEdited;
+    }
+}
